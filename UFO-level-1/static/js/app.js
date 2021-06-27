8562abc1763394d3be3e116153bdeff97648e5b2
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-06-26 20:57:49 -0500

    Added infor for table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Select tbody
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(UFOSightings) {
+    console.log(UFOSightings);
+    // Male table rows 
+     var row = tbody.append("tr");
+
+Object.entries(UFOSightings).forEach(function([key, value]) {
+    console.log(key, value);
+    
+    // Make space for table data 
+    var cell = row.append("td");
+    
+    // Add values 
+    cell.text(value);
+});
+});
